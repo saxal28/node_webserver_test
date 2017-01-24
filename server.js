56044daa1e43fd294d161b99bd02b2b263e80df7
alan <saxal28@gmail.com>
2017-01-24 10:44:26 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials")
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if(err) {
+      console.log("unable to append to sever log");
+    }
+  });
+  next();
+})
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// })
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello Express!! O_o</h1>");
+  res.render("home.hbs", {
+    pageTitle: "Welcome to The Home Page",
+    welcomeMessage: "You are a stupid head",
+  })
+});
+
+app.get("/about", (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About Page"
+  });
+});
+
+// /bad
+app.get("/bad", (req, res) => {
+  res.send({
+    error: "PAGE NOT FOUND"
+  })
+})
+
+app.listen(8080, () => {
+  console.log('server started')
+});
